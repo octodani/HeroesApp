4836504ff0ceaff7db61b373a4c8a3406829ceb7
Daniel Miranda <dmt.791@gmail.com>
2022-08-19 20:24:57 +0200

    tercer commit

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+
+
+
+@NgModule({
+  declarations: [
+    AgregarComponent,
+    BuscarComponent,
+    HeroeComponent,
+    HomeComponent,
+    ListadoComponent
+  ],
+  imports: [
+    CommonModule,
+    HeroesRoutingModule
+  ]
+})
+export class HeroesModule { }
