f585a779f3191fde0be590cc3d69eb5654d3d836
Daniel Miranda <dmt.791@gmail.com>
2022-08-24 12:36:41 +0200

    primer commit

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+
+import { MaterialModule } from '../material/material.module';
 
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
@@ -7,6 +11,8 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
 
 
 
@@ -16,11 +22,16 @@ import { ListadoComponent } from './pages/listado/listado.component';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListadoComponent
+    ListadoComponent,
+    HeroeTarjetaComponent,
+    ImagenPipe
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    FlexLayoutModule,
+    FormsModule,
+    HeroesRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Heroe } from '../../interfaces/heroe.nterface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
+  styles: [`
+  img {
+    width: 100%;
+  }`
   ]
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe!: Heroe;
+
+  constructor(private activatedRoute: ActivatedRoute, private heroesService: HeroesService, private router: Router) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.pipe(switchMap(({id}) => this.heroesService.getHeroePorId(id)))
+      .subscribe(heroe => this.heroe = heroe);
+  }
+
+  volver() {
+    this.router.navigate(['/heroes/listado']);
   }
 
 }
